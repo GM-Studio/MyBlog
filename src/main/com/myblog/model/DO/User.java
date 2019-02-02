05dda49846f284655f455e0968a8a58f39c0d42a
SquirrelChen <827321394@qq.com>
2019-02-02 10:23:15 +0800

    Create this project user things
    Create project user things files
    
    Create by Squirrel Chen
    Edit By 1/2/2019

---
+++ src/main/com/myblog/model/DO/User.java
@@ -0,0 +1,81 @@
+package com.myblog.model.DO;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -3631133546163280304L;
+    private long user_id;
+    private String user_name;
+    private String user_real_name;
+    private String user_sex;
+    private Date user_birth;
+    private String user_addr;
+    private String user_pass;
+    private String user_desc;
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getUser_real_name() {
+        return user_real_name;
+    }
+
+    public void setUser_real_name(String user_real_name) {
+        this.user_real_name = user_real_name;
+    }
+
+    public String getUser_sex() {
+        return user_sex;
+    }
+
+    public void setUser_sex(String user_sex) {
+        this.user_sex = user_sex;
+    }
+
+    public Date getUser_birth() {
+        return user_birth;
+    }
+
+    public void setUser_birth(Date user_birth) {
+        this.user_birth = user_birth;
+    }
+
+    public String getUser_addr() {
+        return user_addr;
+    }
+
+    public void setUser_addr(String user_addr) {
+        this.user_addr = user_addr;
+    }
+
+    public String getUser_pass() {
+        return user_pass;
+    }
+
+    public void setUser_pass(String user_pass) {
+        this.user_pass = user_pass;
+    }
+
+    public String getUser_desc() {
+        return user_desc;
+    }
+
+    public void setUser_desc(String user_desc) {
+        this.user_desc = user_desc;
+    }
+}
