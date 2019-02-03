94522ad01e501e23a7a3de65d3d3f510634aa629
SquirrelChen <827321394@qq.com>
2019-02-03 11:17:51 +0800

    Edit the front end files
    Edit by Squirrel Chen
    Edit By 3/2/2019

--- src/main/webapp/blog/assets/js/index.js
+++ src/main/webapp/blog/assets/js/index.js
@@ -1,28 +1,60 @@
 $(document).ready(function () {
 
+    // $.ajax({
+    //     url:"/tag",
+    //     method:"GET",
+    //     data:{},
+    //     dataType:"json",
+    //     success:function (data) {
+    //
+    //         if(data.code==200)
+    //         {
+    //             var tag_cloud=$("#tag-cloud");
+    //             tag_cloud.empty();
+    //             $.each(data.data,function (i,x) {
+    //                     console.log("hello"+x.tag_id);
+    //                     tag_cloud.append("<a href=\"\" class=\"blog-tag\">"+x.tag_desc+"</a>");
+    //             })
+    //         }
+    //         else if(data.code==-1)
+    //         {
+    //             console.log("数据库操作异常");
+    //         }
+    //     },
+    //     error:function () {
+    //        console.log("ajax请求异常");
+    //     }
+    // })
+
+
+
     $.ajax({
-        url:"/tag",
-        method:"GET",
+        url:"/user/1",
+        method:"get",
         data:{},
         dataType:"json",
-        success:function (data) {
-
+        success:function(data)
+        {
             if(data.code==200)
             {
-                var tag_cloud=$("#tag-cloud");
-                tag_cloud.empty();
-                $.each(data.data,function (i,x) {
-                        console.log("hello"+x.tag_id);
-                        tag_cloud.append("<a href=\"\" class=\"blog-tag\">"+x.tag_desc+"</a>");
+                console.log("请求到数据");
+                var myinfo=$("#myinfo");
+                myinfo.empty();
+                myinfo.append("            <h2 class=\"blog-text-center blog-title\"><span>About ME</span></h2>\n");
+                myinfo.append("            <img src=\"assets/i/f14.jpg\" alt=\"about me\" class=\"blog-entry-img\" >\n");
+                myinfo.append("            <p>"+data.data.userName+"</p>\n");
+                myinfo.append("  <p>\n" +data.data.userDesc+"</p>");
+                var mytags=$("#mytags");
+                mytags.empty();
+                $.each(data.data.userTags,function (i,x) {
+                    mytags.append("<a class=\"blog-tag\">"+x.tagName+"</a>\n");
                 })
             }
-            else if(data.code==-1)
-            {
-                console.log("数据库操作异常");
-            }
         },
-        error:function () {
-           console.log("ajax请求异常");
+        error:function (data) {
+            console.log("ajax请求失败");
         }
     })
+
+
 })
--- src/main/webapp/jsp/index.jsp
+++ src/main/webapp/jsp/index.jsp
@@ -233,7 +233,7 @@
     </div>
 
     <div class="am-u-md-4 am-u-sm-12 blog-sidebar">
-        <div class="blog-sidebar-widget blog-bor">
+        <div class="blog-sidebar-widget blog-bor" id="myinfo">
             <h2 class="blog-text-center blog-title"><span>About ME</span></h2>
             <img src="assets/i/f14.jpg" alt="about me" class="blog-entry-img" >
             <p>妹纸</p>
@@ -241,6 +241,20 @@
                 我是妹子UI，中国首个开源 HTML5 跨屏前端框架
             </p><p>我不想成为一个庸俗的人。十年百年后，当我们死去，质疑我们的人同样死去，后人看到的是裹足不前、原地打转的你，还是一直奔跑、走到远方的我？</p>
         </div>
+
+        <div class="blog-clear-margin blog-sidebar-widget blog-bor am-g " >
+            <h2 class="blog-title"><span>My Tags</span></h2>
+            <div class="am-u-sm-12 blog-clear-padding"id="mytags">
+                <a href="" class="blog-tag">amaze</a>
+                <a href="" class="blog-tag">妹纸 UI</a>
+                <a href="" class="blog-tag">HTML5</a>
+                <a href="" class="blog-tag">这是标签</a>
+                <a href="" class="blog-tag">Impossible</a>
+                <a href="" class="blog-tag">开源前端框架</a>
+            </div>
+        </div>
+
+
         <div class="blog-sidebar-widget blog-bor">
             <h2 class="blog-text-center blog-title"><span>Contact ME</span></h2>
             <p>
@@ -303,6 +317,7 @@
 
 <!--[if (gte IE 9)|!(IE)]><!-->
 <script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/index.js"></script>
 <!--<![endif]-->
 <!--[if lte IE 8 ]>
 <script src="http://libs.baidu.com/jquery/1.11.3/jquery.min.js"></script>
